ee0cb2e3faacc64a2ccf97538f22fbe1905de9af
Rengan Elizabeth <miss.elizaveta.rengan@gmail.com>
2018-11-05 19:48:42 +0200

    Create Progect

---
+++ EntertainmentAgency/EntertainmentAgency/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EntertainmentAgency.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EntertainmentAgency/EntertainmentAgency/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntertainmentAgency.WebApiApplication" Language="C#" %>
