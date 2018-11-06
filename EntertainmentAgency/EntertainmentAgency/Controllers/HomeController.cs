a85890403ee45a31a700303b46ba710a1e24974e
Renzhyna <Renzhyna@A26COMP3>
2018-11-06 17:35:36 +0200

    Add commit

--- EntertainmentAgency/EntertainmentAgency/Controllers/HomeController.cs
+++ EntertainmentAgency/EntertainmentAgency/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntertainmentAgency.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,5 +15,17 @@ namespace EntertainmentAgency.Controllers
 
             return View();
         }
+        public PartialViewResult _PartialMenuView()
+        {
+            List<EntertainmentAgency.Models.Menu> m;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                m = db.Menu.ToList();
+            }
+            return PartialView(m);
+        }
+        
     }
+
+
 }
