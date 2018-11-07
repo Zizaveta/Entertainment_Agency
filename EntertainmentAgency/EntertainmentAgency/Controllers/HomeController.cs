67b0d0d69e30ec5611f2e864e3f5a3104cec4afe
Renzhyna <Renzhyna@A26COMP3>
2018-11-07 17:56:52 +0200

    Added Views ans Controller

--- EntertainmentAgency/EntertainmentAgency/Controllers/HomeController.cs
+++ EntertainmentAgency/EntertainmentAgency/Controllers/HomeController.cs
@@ -20,11 +20,37 @@ namespace EntertainmentAgency.Controllers
             List<EntertainmentAgency.Models.Menu> m;
             using (ApplicationContext db = new ApplicationContext())
             {
-                m = db.Menu.ToList();
+                m = db.Menu.OrderBy(elem => elem.Name).ToList();
             }
             return PartialView(m);
         }
-        
+        public PartialViewResult _PartialDesignView()
+        {
+            List<EntertainmentAgency.Models.Design> m;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                m = db.Designes.OrderBy(elem => elem.typeOfEntertainment.Type).ToList();
+            }
+            return PartialView(m);
+        }
+        public PartialViewResult _PartialTypesView()
+        {
+            List<EntertainmentAgency.Models.TypeOfEntertainment> m;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                m = db.TypeOfEntertainments.OrderBy(elem => elem.Type).ToList();
+            }
+            return PartialView(m);
+        }
+        public PartialViewResult _PartialCompetitionView()
+        {
+            List<EntertainmentAgency.Models.Competition> m;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                m = db.Competitions.OrderBy(elem => elem.typeOfEntertainment.Type).ToList();
+            }
+            return PartialView(m);
+        }
     }
 
 
---
+++ EntertainmentAgency/EntertainmentAgency/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using EntertainmentAgency.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntertainmentAgency.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+        public PartialViewResult _PartialTypesView()
+        {
+            List<EntertainmentAgency.Models.TypeOfEntertainment> m;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                m = db.TypeOfEntertainments.OrderBy(elem => elem.Type).ToList();
+            }
+            return PartialView(m);
+        }
+    }
+}
